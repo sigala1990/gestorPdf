72f1147a65c51ccd3998f3eea3533a8ed7cc6ecc
adriaqf <adriaqf@gmail.com>
2022-07-27 20:40:39 +0200

    aa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,75 @@
-import { Component } from '@angular/core';
-
+import { Component, Input } from '@angular/core';
+import { HttpClient, HttpErrorResponse, HttpEventType } from '@angular/common/http';
+import { finalize, Subscription } from 'rxjs';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'gestorPdf';
+
+  myForm = new FormGroup({
+
+    name: new FormControl('', [Validators.required, Validators.minLength(3)]),
+
+    file: new FormControl('', [Validators.required]),
+
+    fileSource: new FormControl('', [Validators.required])
+
+  });
+
+
+  constructor(private http: HttpClient) { }
+
+
+  get f(){
+
+    return this.myForm.controls;
+
+  }
+
+
+
+
+  onFileChange(event:any) {
+
+
+
+    if (event.target.files.length > 0) {
+
+      const file = event.target.files[0];
+
+      this.myForm.patchValue({
+
+        fileSource: file
+
+      });
+
+    }
+
+  }
+
+
+
+
+  submit(){
+
+    const formData = new FormData();
+if( this.myForm.get('fileSource')?.value !== null){
+  formData.append('file', this.myForm.get('fileSource').value);
+}
+
+
+    this.http.post('http://localhost:8001/upload.php', formData)
+
+      .subscribe(res => {
+
+        console.log(res);
+
+        alert('Uploaded Successfully.');
+
+      })
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
